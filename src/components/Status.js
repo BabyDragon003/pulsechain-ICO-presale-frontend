3e0d56015d6be215ed7c99a19b0302829d4d14db
BabyDragon003 <golddragon01234@gmail.com>
2023-03-01 18:54:52 +0900

    "2023-03-01 18:54"

--- src/components/Status.js
+++ src/components/Status.js
@@ -3,10 +3,8 @@ import StatusItem from "./StatusItem";
 export default function Status(props) {
     return (
         <div className="w-full mx-auto gap-4 flex lg:flex-row flex-col items-center justify-between text-center px-1 py-3">
-                plsAmountFor1USD={props.plsAmountFor1USD}
-                projectTokenBalance={props.projectTokenBalance}
-                nextRoundStartTime={props.nextRoundStartTime}
-            />
-        </div>
-    );
-}
+            <StatusItem
+                totalSoldAmount={props.totalSoldAmount}
+                totalFundsInUSD={props.totalFundsInUSD}
+                roundNumber={props.roundNumber}
+                currentTokenPrice={props.currentTokenPrice}
