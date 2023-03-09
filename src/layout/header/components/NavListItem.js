228b5994dd560c51cac1cd39d3ff19b61fea9f92
BabyDragon003 <golddragon01234@gmail.com>
2023-03-09 18:54:52 +0900

    "2023-03-09 18:54"

--- src/layout/header/components/NavListItem.js
+++ src/layout/header/components/NavListItem.js
@@ -13,21 +13,5 @@ export default function NavListItem(props) {
           <Link
             key={menu.label}
             to={menu.link}
-            target={menu.target !== undefined ? menu.target : "_self"}
-            onClick={() => {
-              if (menu.self) {
-                window.location.replace(menu.link);
-              }
-            }}
-          >
-            <MenuItem className="flex items-center gap-2 !text-trumpos-fontDark hover:!text-trumpos-fontLight hover:underline hover:underline-offset-4 !bg-transparent lg:rounded-full">
-              <Typography as="p" variant="h4" className="font-menu" color="yellow">
-                {menu.label}
-              </Typography>
-            </MenuItem>
-          </Link>
-        )}
-      </ul>
-    </React.Fragment>
   );
 }
