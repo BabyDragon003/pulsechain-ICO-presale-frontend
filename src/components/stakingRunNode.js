dbe40e5decf925b4040b9589a3cef3e402ebba8d
BabyDragon003 <golddragon01234@gmail.com>
2023-03-17 18:54:52 +0900

    "2023-03-17 18:54"

--- src/components/stakingRunNode.js
+++ src/components/stakingRunNode.js
@@ -13,16 +13,11 @@ export default function StakingRunNode() {
                                 src={"https://rocketpool.net/assets/services-panel-node-bab61c83.webp"}
                             />
                         </div>
-                                            className="svg-inline--fa fa-network-wired fa-3x h-10 w-10"
-                                            aria-hidden="true"
-                                            focusable="false"
-                                            data-prefix="fas"
-                                            data-icon="network-wired"
-                                            role="img"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 640 512"
-                                        >
-                                            <path
+                        <h2
+                            className="py-10 text-center text-4xl font-bold tracking-tight sm:text-6xl bg-gradient-to-r from-red-500 via-yellow-400 to-red-400 bg-clip-text text-transparent leading-8"
+                        >
+                            Stake + Run Node
+                        </h2>
                                                 className=""
                                                 fill="currentColor"
                                                 d="M256 64H384v64H256V64zM240 0c-26.5 0-48 21.5-48 48v96c0 26.5 21.5 48 48 48h48v32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96v32H80c-26.5 0-48 21.5-48 48v96c0 26.5 21.5 48 48 48H240c26.5 0 48-21.5 48-48V368c0-26.5-21.5-48-48-48H192V288H448v32H400c-26.5 0-48 21.5-48 48v96c0 26.5 21.5 48 48 48H560c26.5 0 48-21.5 48-48V368c0-26.5-21.5-48-48-48H512V288h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H352V192h48c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H240zM96 448V384H224v64H96zm320-64H544v64H416V384z"
